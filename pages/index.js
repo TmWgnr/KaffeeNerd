673261221408983504ca6ca288ee0c7d3be594c1
Tim Wagner <tim.w@gner-mail.de>
2023-06-19 16:17:56 +0200

    add listpage in pages, WelcomePage & BackgroundImage component

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <main>
       <Heading>Kaffeesorten</Heading>
-      <CoffeeList />
     </main>
   );
 }
---
+++ pages/listpage/index.js
@@ -0,0 +1,11 @@
+import CoffeeList from "../../components/CoffeeList";
+import Heading from "../../components/Heading";
+
+export default function ListPage() {
+  return (
+    <main>
+      <Heading>Kaffeesorten</Heading>
+      <CoffeeList />
+    </main>
+  );
+}
