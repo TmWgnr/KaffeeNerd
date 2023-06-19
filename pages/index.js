f3660823800b1bf6bf8d3153498e21659707f7f6
Tim Wagner <tim.w@gner-mail.de>
2023-06-19 09:57:16 +0200

    delete subheading and change heading

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import Heading from "../components/Heading";
-import SubHeading from "../components/SubHeading";
 
 export default function Home() {
   return (
     <main>
-      <Heading>🐬 Capstone Template 🐬</Heading>
-      <SubHeading>We dolphinitely love coding!</SubHeading>
+      <Heading>KaffeeListe</Heading>
     </main>
   );
 }
