1f074b23a27768c2db6ab1a4243da766a811cbbc
Tim Wagner <tim.w@gner-mail.de>
2023-06-21 10:58:31 +0200

    add HomeButton component and implemnt on footer at homepage

---
+++ components/HomeButton/index.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { useRouter } from "next/router";
+
+const StyledButton = styled.button`
+  background-color: transparent;
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 15px 32px;
+  margin: 5px 5px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 16px;
+`;
+
+export default function HomeButton() {
+  const router = useRouter();
+
+  function handleClick() {
+    router.push("/pages");
+  }
+
+  return <StyledButton onClick={handleClick}>Home</StyledButton>;
+}
