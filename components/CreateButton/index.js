eb2aec2280da5fa95bc65bfb776202a894fadd7e
Tim Wagner <tim.w@gner-mail.de>
2023-07-11 10:13:53 +0200

    make reload after deletion, change footer styling & delte console.logs

--- components/CreateButton/index.js
+++ components/CreateButton/index.js
@@ -2,16 +2,15 @@ import styled from "styled-components";
 import { useRouter } from "next/router";
 
 const StyledButton = styled.button`
-  background-color: ${(page) => (page.active ? "#d3d3d3" : "#000000")};
-  border: solid transparent 1px;
-  color: ${(page) => (page.active ? "#000000" : "#d3d3d3")};
-  opacity: 0.7;
-  border-radius: 5px;
+  color: ${(page) => (page.active ? "#A9A9A9" : "#000000")};
+
+  background-color: transparent;
+  border: none;
   width: 100px;
   height: 50px;
   text-align: center;
-  text-decoration: none;
   font-size: 16px;
+  text-decoration: ${(page) => (page.active ? "underline" : "none")};
 `;
 
 export default function CreateButton2() {
@@ -24,20 +23,7 @@ export default function CreateButton2() {
 
   return (
     <StyledButton active={isActive} onClick={handleClick}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="15"
-        height="15"
-        viewBox="0 0  20 20"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <line x1="12" y1="5" x2="12" y2="19" />
-        <line x1="5" y1="12" x2="19" y2="12" />
-      </svg>
+      +
     </StyledButton>
   );
 }
--- components/ListButton/index.js
+++ components/ListButton/index.js
@@ -2,22 +2,19 @@ import styled from "styled-components";
 import { useRouter } from "next/router";
 
 const StyledButton = styled.button`
-  background-color: ${(page) => (page.active ? "#d3d3d3" : "#000000")};
-  border: solid transparent 1px;
-  color: ${(page) => (page.active ? "#000000" : "#d3d3d3")};
-  opacity: 0.7;
-  border-radius: 5px;
+  color: ${(page) => (page.active ? "#A9A9A9" : "#000000")};
+  background-color: transparent;
+  border: none;
   width: 100px;
   height: 50px;
-  margin: 5px 5px;
   text-align: center;
-  text-decoration: none;
   font-size: 16px;
+  text-decoration: ${(page) => (page.active ? "underline" : "none")};
 `;
 
 export default function ListButton() {
   const router = useRouter();
-  console.log(router.pathname);
+
   const isActive = router.pathname === "/listpage";
 
   function handleClick() {
